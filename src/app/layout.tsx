e7ccde7acbfee68cfc84c2b173713063d9e4c8bd
Leon Wu <leon.wuuu@gmail.com>
2023-08-04 11:23:17 +1000

    added create community functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-
 
 export const metadata = {
   title: "Breadit",
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  authModal
+  authModal,
 }: {
   children: React.ReactNode;
   authModal: React.ReactNode;
@@ -27,17 +27,20 @@ export default function RootLayout({
         inter.className
       )}
     >
-      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <Navbar />
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
 
-        {authModal}
+          {authModal}
 
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-          {children}
-        </div>
-        
-        <Toaster />
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+
+          <Toaster />
+          
+        </Providers>
       </body>
     </html>
   );
