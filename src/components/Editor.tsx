b4b5637e7853c755503649106efe61103da6a567
Leon Wu <leon.wuuu@gmail.com>
2023-09-19 12:09:34 +1000

    implmented editor, fixed links not working

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -28,13 +28,13 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   });
 
   const ref = useRef<EditorJS>();
-  const [isMounter, setIsMounter] = useState<boolean>(false)
+  const [isMounted, setIsMounted] = useState<boolean>(false)
 
   useEffect(() => {
     if(typeof window !== 'undefined') {
-      setIsMounter(true)
+      setIsMounted(true)
     }
-  })
+  }, [])
   
 
   const initializeEditor = useCallback(async () => {
@@ -62,7 +62,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
           linkTool: {
             class: LinkTool,
             config: {
-              endpoint: 'api/link'
+              endpoint: '/api/link'
             },
           },
           image: {
@@ -95,6 +95,23 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor()
+
+      setTimeout(() => {
+        // set focus to title
+      })
+    }
+
+    if(isMounted) {
+      init()
+
+      return () => {}
+    }
+
+  }, [isMounted, initializeEditor])
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit=post-form" className="w-fit" onSubmit={() => {}}>
@@ -103,6 +120,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
           placeholder="Title"
           className="w-full resize-none appearace-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
         />
+        <div id='editor' className="min-h-[500px]"></div>
       </form>
     </div>
   );
