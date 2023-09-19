f21eb27815baf5bf67df29519c28ea6a0b22f19c
Leon Wu <leon.wuuu@gmail.com>
2023-09-19 11:43:25 +1000

    implemented uploadthing

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const Layout = async ({
@@ -92,6 +94,11 @@ const Layout = async ({
               {subreddit.creatorId !== session?.user.id ? (
                 <SubscribeLeaveToggle subredditId={subreddit.id} subredditName={subreddit.name} isSubscribed={isSubscribed}/>
               ) : null}
+
+              <Link className={buttonVariants({
+                variant: 'outline',
+                className: 'w-full mb-4',
+              })} href={`r/${slug}/submit`}>Create Post</Link>
             </dl>
           </div>
         </div>
---
+++ src/components/Editor.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import React, { FC, useCallback, useEffect, useRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { useForm } from "react-hook-form";
+import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type EditorJS from "@editorjs/editorjs";
+import { uploadFiles } from "@/lib/uploadthing";
+
+
+interface EditorProps {
+  subredditId: string;
+}
+
+const Editor: FC<EditorProps> = ({ subredditId }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subredditId,
+      title: "",
+      content: null,
+    },
+  });
+
+  const ref = useRef<EditorJS>();
+  const [isMounter, setIsMounter] = useState<boolean>(false)
+
+  useEffect(() => {
+    if(typeof window !== 'undefined') {
+      setIsMounter(true)
+    }
+  })
+  
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import("@editorjs/editorjs")).default;
+    const Header = (await import("@editorjs/header")).default;
+    const Embed = (await import("@editorjs/embed")).default;
+    const Table = (await import("@editorjs/table")).default;
+    const List = (await import("@editorjs/list")).default;
+    const Code = (await import("@editorjs/code")).default;
+    const LinkTool = (await import("@editorjs/link")).default;
+    const InlineCode = (await import("@editorjs/inline-code")).default;
+    const ImageTool = (await import("@editorjs/image")).default;
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: "editor",
+        onReady() {
+          ref.current = editor;
+        },
+        placeholder: "Type here to write your post...",
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: 'api/link'
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles({
+                    endpoint: "imageUploader",
+                    files: [file],
+                  })
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.url,
+                    },
+                  }
+                }
+              }
+            }
+          },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
+        },
+      });
+    }
+  }, []);
+
+  return (
+    <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
+      <form id="subreddit=post-form" className="w-fit" onSubmit={() => {}}>
+        <div className="prose prose-stone dark:prose-invert"></div>
+        <TextareaAutosize
+          placeholder="Title"
+          className="w-full resize-none appearace-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Editor;
