fb71f4ee067150930a8cee5df3ea5e5402c45eaa
Leon Wu <leon.wuuu@gmail.com>
2023-09-22 14:10:08 +1000

    added home feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import CustomFeed from "@/components/CustomFeed";
+import GeneralFeed from "@/components/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
+import { getAuthSession } from "@/lib/auth";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getAuthSession()
+
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* feed */}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
 
@@ -23,7 +30,7 @@ export default function Home() {
         <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
           <div className="flex justify-between gap-x-4 py-3">
             <p className="text-zinc-500">
-              Your personal Breadit hmepage. Come here to check in with your favourite communities
+              Your personal Breadit homepage. Come here to check in with your favourite communities
             </p>
           </div>
 
