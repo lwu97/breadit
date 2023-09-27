05efb5294aed662a94568644680ba184f16b6d28
Leon Wu <leon.wuuu@gmail.com>
2023-09-27 16:13:47 +1000

    added search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const Navbar = async () => {
 
@@ -18,6 +19,7 @@ const session = await getAuthSession()
             </Link>
 
             {/* search bar */}
+            <SearchBar />
 
             {session?.user ? (
               <UserAccountNav user={session.user} />
